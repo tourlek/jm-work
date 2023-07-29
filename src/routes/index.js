31b1a94d0e64f6fd00d752752711b80ee4ffccbb
lek <stp.rodkeaw@gmail.com>
2023-07-29 20:50:03 +0700

    update theam and logic function

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,24 +4,34 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
+      path: "/home",
       name: "home",
       component: () => import("../views/home.vue"),
+      meta: { requiresAuth: true },
     },
     {
       path: "/list-employee",
       name: "list-employee",
       component: () => import("../views/employee/index.vue"),
+      meta: { requiresAuth: true },
     },
     {
       path: "/edit-employee/:id",
       name: "edit-list-employee",
       component: () => import("../views/employee/edit/index.vue"),
+      meta: { requiresAuth: true },
     },
     {
       path: "/edit-employer/:id",
       name: "edit-employer",
       component: () => import("../views/employer/index.vue"),
+      meta: { requiresAuth: true },
+    },
+    {
+      path: "/profile",
+      name: "profile",
+      component: () => import("../views/profile/index.vue"),
+      meta: { requiresAuth: true },
     },
   ],
 });
