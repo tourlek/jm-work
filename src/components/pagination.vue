190a9f4279b54dfee195c2be5c62380508f36084
lek <stp.rodkeaw@gmail.com>
2023-08-20 11:58:19 +0700

    update code

---
+++ src/components/pagination.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from "vue";
+defineProps({
+  msg: String,
+  type: String,
+  deleteBtn: Boolean,
+  modal: Boolean,
+});
+
+const emit = defineEmits(["click"]);
+
+const limit = ref(10);
+const page = ref(1);
+const totalPages = ref(3); // This will be updated when data is fetched
+
+const changePage = (pageNumber) => {
+  page.value = pageNumber;
+  console.log(pageNumber);
+  emit("click", pageNumber);
+};
+</script>
+<template>
+  <div class="join">
+    <button
+      v-for="pageNumber in totalPages"
+      :key="pageNumber"
+      :class="['join-item btn', page === pageNumber ? 'btn-active' : '']"
+      @click="changePage(pageNumber)"
+    >
+      {{ pageNumber }}
+    </button>
+  </div>
+</template>
--- src/views/employee/index.vue
+++ src/views/employee/index.vue
@@ -3,17 +3,21 @@ import { ref, computed, onMounted } from "vue";
 import Table from "@/components/table.vue";
 import { useEmployeeStore } from "@/stores/employee.js";
 import Loading from "@/components/loading.vue";
-
+import Paginate from "@/components/pagination.vue";
+let pageRef = ref(1);
 let loading = ref(true);
 let store = useEmployeeStore();
 let data = computed(() => store.employee);
-
+const changePage = async (page) => {
+  await store.fetchEmployees(page, 4);
+};
 onMounted(async () => {
-  await store.fetchEmployees();
+  await store.fetchEmployees(pageRef.value, 4);
   loading.value = false;
 });
 </script>
 <template>
   <div v-if="loading"><Loading /></div>
   <Table :data="data" />
+  <!-- <Paginate @click="changePage" /> -->
 </template>
